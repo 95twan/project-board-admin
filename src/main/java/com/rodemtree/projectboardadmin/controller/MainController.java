085386cc8c9234f3ae0212d93966615a005b77b8
김태완 <95twan@naver.com>
2025-07-19 22:32:33 +0900

    #5 - 뷰 컨트롤러, 기본 뷰, 테스트 기초 작성

---
+++ src/main/java/com/rodemtree/projectboardadmin/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.rodemtree.projectboardadmin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String root() {
+        return "forward:/management/articles";
+    }
+
+}
