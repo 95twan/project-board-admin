bc2edc562efd85226c4b9644fc521a8346ca5591
김태완 <95twan@naver.com>
2025-07-22 18:59:39 +0900

    #14 - 어드민 회원 서비스 코드 작성과 테스트

---
+++ src/main/java/com/rodemtree/projectboardadmin/service/AdminAccountService.java
@@ -0,0 +1,42 @@
+package com.rodemtree.projectboardadmin.service;
+
+import com.rodemtree.projectboardadmin.domain.constant.RoleType;
+import com.rodemtree.projectboardadmin.dto.AdminAccountDto;
+import com.rodemtree.projectboardadmin.repository.AdminAccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Service
+public class AdminAccountService {
+
+    private final AdminAccountRepository adminAccountRepository;
+
+    public Optional<AdminAccountDto> searchUser(String username) {
+        return Optional.empty();
+    }
+
+    public AdminAccountDto saveUser(
+            String username,
+            String password,
+            Set<RoleType> roleTypes,
+            String email,
+            String nickname,
+            String memo
+    ) {
+        return null;
+    }
+
+    public List<AdminAccountDto> users() {
+        return List.of();
+    }
+
+    public void deleteUser(String username) {
+
+    }
+
+}
